3daffffb07ca43c3e3b310d1ed0260c99cd717d4
wuhailing <573058815@qq.com>
2021-04-24 18:40:50 +0800

    首页 布局

---
+++ components/home/beancurd-cube/index.js
@@ -0,0 +1,67 @@
+
+Component({
+  timer: null,
+
+  properties: {
+
+  },
+
+  data: {
+    scrollListWidth: 0,
+    list: [
+      [{
+        id: 1,
+      },{
+        id: 2,
+      },{
+        id: 3,
+      },{
+        id: 4,
+      },{
+        id: 5,
+      },{
+        id: 6,
+      },{
+        id: 7,
+      },{
+        id: 8,
+      },{
+        id: 9,
+      },{
+        id: 10,
+      }],
+      [{
+        id: 11,
+      },{
+        id: 12,
+      },{
+        id: 13,
+      },{
+        id: 14,
+      },{
+        id: 15,
+      },{
+        id: 16,
+      }]
+    ]
+  },
+
+  methods: {
+    handleScroll({
+      detail
+    }) {
+      let {
+        scrollListWidth,
+      } = this.data;
+      const {
+        scrollLeft,
+        scrollWidth,
+      } = detail;
+      scrollListWidth = parseInt(44 * (scrollLeft / (scrollWidth / 2)));
+      // scrollListWidth = 44 * (scrollLeft / (scrollWidth / 2));
+      this.setData({
+        scrollListWidth,
+      })
+    }
+  }
+})
---
+++ components/home/coupon/index.js
@@ -0,0 +1,19 @@
+import { IMG_CDN } from '../../../constants/common'
+
+Component({
+  properties: {
+
+  },
+
+  data: {
+    coupon: `${IMG_CDN}miniprogram/home/coupon.png`,
+    couponSelect: `${IMG_CDN}miniprogram/home/coupon_select.png`,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
