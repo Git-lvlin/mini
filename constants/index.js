09ea73b3e80bcc09752ef7b08cce26f1bd465194
wuhailing <573058815@qq.com>
2021-09-13 22:21:17 +0800

    1.0.3 紧急修复下单问题

--- constants/index.js
+++ constants/index.js
@@ -10,7 +10,7 @@ export const SYS_ENV = 'pro';
 export const CHANGE_ENV = false;
 
 // 小程序版本号
-export const VERSION = "1.0.2";
+export const VERSION = "1.0.3";
 
 // 服务器接口域名S
 export const baseApi = {
