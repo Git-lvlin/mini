7fdc7bbb510dcfd545479e375145e6d0005aa211
wuhailing <573058815@qq.com>
2021-10-26 16:43:26 +0800

    签到页面调整完毕

--- apis/activity.js
+++ apis/activity.js
@@ -1,8 +1,10 @@
 import Request from '../utils/request'
 
 const url = {
-  signInfo: "/public/auth/userSign/getSignInfo",
-  sign: "/public/auth/userSign/sign",
+  signInfo: '/public/auth/userSign/getSignInfo',
+  sign: '/public/auth/userSign/sign',
+  queryRecord: '/integral/auth/redPacketRecord/queryRecordList',
+  signGood: '/integral/open/redPacketProd/page',
 }
 
 export default {
@@ -13,5 +15,13 @@ export default {
   // 签到
   userSign(params, option) {
     return Request.post(url.sign, params, option);
+  },
+  // 获取红包明细
+  getQueryRecord(params, option) {
+    return Request.post(url.queryRecord, params, option);
+  },
+  // 获取签到商品列表
+  getSignGood(params, option) {
+    return Request.get(url.signGood, params, option);
   },
 }
