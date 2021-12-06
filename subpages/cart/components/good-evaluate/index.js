0bc2d62e6a4cc2ddce305dd79a165d885210f132
wuhailing <573058815@qq.com>
2021-12-06 16:56:58 +0800

    评价UI

--- subpages/cart/components/good-evaluate/index.js
+++ subpages/cart/components/good-evaluate/index.js
@@ -1,4 +1,4 @@
-
+import router from '../../../../utils/router'
 
 Component({
   options: {
@@ -9,17 +9,16 @@ Component({
 
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    onToEvaluate() {
+      router.push({
+        name: 'evaluate',
+        data: {},
+      });
+    },
   }
 })
