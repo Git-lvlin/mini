d48ae0942f7d9daa447b219aad7ebd03d51644f5
huyueke <huyueke@yongdaxing.cn>
2022-05-10 17:10:19 +0800

    fix

--- pages/location/index.js
+++ pages/location/index.js
@@ -408,6 +408,7 @@ create.Page(store, {
     //   tempCity = `${province}${city}${inputText}`
     // }
     let querykeywords = inputText;
+    console.log('querykeywords', querykeywords)
     myAmapFun.getPoiAround({
       querykeywords,
       location: this.selectLocation,
